9d561cfb4e45b6d138360cc351cc7014e2fe0088
srijan39 <heresrijan@gmail.com>
2026-06-18 10:14:07 +0530

    register wishlist provider

--- ecommerce-frontend/src/main.tsx
+++ ecommerce-frontend/src/main.tsx
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App.tsx";
 import { CartProvider } from "./context/CartContext";
 import { AuthProvider } from "./context/AuthContext";
+import { WishlistProvider } from "./context/WishlistContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
       <CartProvider>
-        <App />
+        <WishlistProvider>
+          <App />
+        </WishlistProvider>
       </CartProvider>
     </AuthProvider>
   </StrictMode>
